70c0d4b1c248aeb977562ef2febbf8d9ae62027f
ruixingchen <dev@ruixingchen.com>
2020-03-04 00:19:10 +0800

    Initial Commit

---
+++ RXCPageViewController/RXCPageViewController.h
@@ -0,0 +1,19 @@
+//
+//  RXCPageViewController.h
+//  RXCPageViewController
+//
+//  Created by ruixingchen on 2020/3/4.
+//  Copyright © 2020 ruixingchen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RXCPageViewController.
+FOUNDATION_EXPORT double RXCPageViewControllerVersionNumber;
+
+//! Project version string for RXCPageViewController.
+FOUNDATION_EXPORT const unsigned char RXCPageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RXCPageViewController/PublicHeader.h>
+
+
